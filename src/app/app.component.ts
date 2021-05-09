1cfe5439d1421a985331e33e64c916585a29dcb5
alhassane85 <alhassanebarry0@gmail.com>
2021-05-09 00:56:41 +0000

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { range } from 'rxjs';
+import { count } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit{
   name = 'Angular';
+  numbers;result;
+  ngOnInit() {
+     // Called after the constructor and called  after the first ngOnChanges() 
+     const numbers = range(1, 7);
+const result = numbers.pipe(count(i => i % 2 === 1));
+  result.subscribe(x => console.log(x));
+  }
+
+
+
 }
